d309f340c50b002884913f8cc364c97b28e4e240
Daniel-FC24 <aforerod@uninorte.edu.co>
2026-04-07 18:16:26 -0500

    Update Content.java

--- src/core/cargolink/logisticscenter/Content.java
+++ src/core/cargolink/logisticscenter/Content.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package core.cargolink.logisticscenter;
+package core;
 
 /**
  *
- * @author valbuenas
+ * @author aforerod
  */
-public class Content {
-    
+public enum Content {
+    DOCUMENTS,
+    ELECTRONICS,
+    CLOTHING,
+    OTHERS
 }
