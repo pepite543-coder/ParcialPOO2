477b815a19469cf8d2214c79afde12be14458b79
Daniel-FC24 <aforerod@uninorte.edu.co>
2026-04-07 18:04:19 -0500

    Update Truck.java

--- src/core/cargolink/vehicle/Truck.java
+++ src/core/cargolink/vehicle/Truck.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package core.cargolink.vehicle;
+package core.Vehicle;
 
 /**
  *
- * @author valbuenas
+ * @author aforerod
  */
-public class Truck {
-    
+public class Plane extends Vehicle {
+   private String licensePlate;
+   
 }
