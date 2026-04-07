2b927c4d7e34777e2403aff295d83008f2f1f6bd
Daniel-FC24 <aforerod@uninorte.edu.co>
2026-04-07 17:55:06 -0500

    Update Shipping.java

--- src/core/cargolink/logisticscenter/Shipping.java
+++ src/core/cargolink/logisticscenter/Shipping.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package core.cargolink.logisticscenter;
-
+import core.Vehicle.Vehicle;
+import java.util.ArrayList;
+import java.util.Date;
 /**
  *
  * @author valbuenas
  */
 public class Shipping {
-    
+private String code;
+private Client client;
+private String originCenter; 
+private String destinationCenter;
+private Date registrationDate;
+private Date deliveryDate;
+private Status status;
+private ArrayList<Package> packages;
+private Vehicle vehicle;   
 }
