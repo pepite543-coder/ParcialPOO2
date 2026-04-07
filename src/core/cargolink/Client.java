8b9cfdfd6a45a56f36d20ffc04190a9d947358eb
Daniel-FC24 <aforerod@uninorte.edu.co>
2026-04-07 17:57:23 -0500

    Update Client.java

--- src/core/cargolink/Client.java
+++ src/core/cargolink/Client.java
@@ -2,16 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package core.cargolink;
+package core;
+
+import java.util.ArrayList;
 
 /**
  *
- * @author valbuenas
+ * @author aforerod
  */
 public class Client {
-private int id;
-private String name;
-private String address;
-private String email;
-private ArrayList<Shipping> shippings;   
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+    private ArrayList<Shipping> shippings;
+    
+    
 }
